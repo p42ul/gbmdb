fb83ec7fb4857d02417d2bb866b55754dd6f180a
Paul Buser <buser.paul@gmail.com>
2022-04-30 16:45:57 -0400

    update functions and CSV input/output

--- gbmdb.py
+++ gbmdb.py
@@ -1,12 +1,13 @@
+import csv
 from collections import defaultdict
 
-def read_dump(filename, target_mode):
+def read_vgm_dump(input_filename):
     raw_data = None
-    with open(filename, 'r') as f:
+    with open(input_filename, 'r') as f:
         raw_data = f.read()
     mode = None
     position = 0
-    ret = defaultdict(dict)
+    data = defaultdict(dict)
     for line in raw_data.splitlines():
         if line.startswith('mode:'):
             mode = line.split()[-1]
@@ -14,9 +15,30 @@ def read_dump(filename, target_mode):
         if line.startswith('VGMSmplPos'):
             position = int(line.split()[-1])
             continue
-        if mode != target_mode:
-            continue
         key, value = line.split()
         value = int(value)
-        ret[position][key] = value
-    return ret
+        data[position][mode + '_' + key] = value
+    return data
+
+def dump2csv(input_filename, output_filename):
+    data = read_vgm_dump(input_filename)
+    attributes = sorted(data[0].keys())
+    with open(output_filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        # Write headers
+        writer.writerow(['smpl'] + attributes)
+        for smpl in sorted(data.keys()):
+            line_data = [data[smpl][k] for k in attributes]
+            writer.writerow([smpl] + line_data)
+
+def csv2dump(input_filename):
+    data = defaultdict(dict)
+    with open(input_filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for i, row in enumerate(reader):
+            if i == 0:
+                headers = row
+                continue
+            smpl = int(row[0])
+            data[smpl] = {headers[ci]: int(ce) for ci, ce in enumerate(row)}
+    return data
